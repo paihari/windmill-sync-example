497e6e12c774881ea00da47acecc9b02c9812596
paihari <paihari@users.noreply.github.com>
2023-08-17 20:04:54 +0000

    [create-pull-request] automated change

--- f/examples/hello_world.py
+++ f/examples/hello_world.py
@@ -1,3 +1,3 @@
 def main(name: str = "Nicolas Bourbaki"):
-	print(f"Hello World and a warm welcome especially to Sync {name}")
+	print(f"Hello World and a warm welcome especially to Sync 2 {name}")
 	return {"len": len(name), "splitted": name.split() }
